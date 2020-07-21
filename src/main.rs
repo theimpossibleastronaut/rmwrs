0321b5d63accce052e02ef6efb9da94667513dac
andy5995 <andy400-dev@yahoo.com>
2020-07-20 22:59:02 -0500

    convert homedir to string, add example StructOpt code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,62 @@
-use std::fs::rename;
+use std::option::Option;
+// use std::fs::rename;
 use structopt::StructOpt;
 use std::path::PathBuf;
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Set speed
+    // we don't want to name it "speed", need to look smart
+    #[structopt(short = "v", long = "velocity", default_value = "42")]
+    speed: f64,
+
+    /// Input file
     #[structopt(parse(from_os_str))]
-    path: PathBuf
+    input: PathBuf,
+
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+
+    /// Where to write the output: to `stdout` or `file`
+    #[structopt(short)]
+    out_type: String,
+
+    /// File name: only required when `out` is set to `file`
+    #[structopt(name = "FILE", required_if("out_type", "file"))]
+    file_name: Option<String>,
 }
 
 fn main() {
+    // https://github.com/openethereum/openethereum/pull/9077/files
+    // https://doc.rust-lang.org/std/option/enum.Option.html#variant.Some
+    /* "Because unwrap() may panic, its use is generally
+    discouraged. Instead, prefer to use pattern matching and handle the
+    None case explicitly, or call unwrap_or, unwrap_or_else, or
+    unwrap_or_default. */
+    let homedir: String = dirs::home_dir().unwrap_or_default().to_str().unwrap().into();
 
-    let _home_dir: Option<PathBuf> = dirs::home_dir();
-    println!("Your home directory: {:?}", _home_dir);
+    println!("Your home directory: {:?}", homedir);
 
     // test code for renaming files
-    rename("temp", "temp_foo");
+    // rename("temp", "temp_foo");
 
-    let args = Cli::from_args();
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 
     // test code for processing cli args
-    let content = std::fs::read_to_string(&args.path)
+    /* let content = std::fs::read_to_string(&args.path)
     .expect("could not read file");
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
-    }
+    } */
 }
