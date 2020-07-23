b951d18d2c31157143c2901edc7323733cb3754e
andy5995 <andy400-dev@yahoo.com>
2020-07-23 00:21:50 -0500

    add test for get_basename()

--- src/main.rs
+++ src/main.rs
@@ -75,3 +75,25 @@ fn main() {
         fs::write(trashinfo_filename, contents).expect("Error writing to file");
     }
 }
+
+#[test]
+fn check_get_basename() {
+    let mut some_basename = PathBuf::from(r"./test/basename/");
+    assert_eq!(get_basename(&some_basename), "basename");
+
+    some_basename = PathBuf::from(r"/./test/basename");
+    assert_eq!(get_basename(&some_basename), "basename");
+
+    // a filename containing spaces
+    some_basename = PathBuf::from(r"/./test/base name/");
+    assert_eq!(get_basename(&some_basename), "base name");
+}
+
+#[test]
+#[ignore]
+fn check_get_basename_windows() {
+    // a file on Windows
+    let mut some_basename = PathBuf::from(r"C:\windows\system32.dll");
+    // Does not work on Linux (and maybe not on Windows either; returns "C:\\windows\\system32.dll")
+    assert_eq!(get_basename(&some_basename), "system32.dll");
+}
