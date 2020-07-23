fd0777b1556b4fa57b6cf33a0e7de65e48f1070b
andy5995 <andy400-dev@yahoo.com>
2020-07-23 15:28:34 -0500

    move tests to separate tests directory

--- src/libgen.rs
+++ src/libgen.rs
@@ -1,28 +1,6 @@
-use std::ffi::{OsStr, OsString};
-use std::path::{Path, PathBuf};
+use std::ffi::OsStr;
+use std::path::Path;
 
 pub fn get_basename(path: &Path) -> &OsStr {
     path.file_name().unwrap_or_else(|| path.as_os_str())
-}
-
-#[test]
-fn check_get_basename() {
-    let mut some_basename = PathBuf::from(r"./test/basename/");
-    assert_eq!(get_basename(&some_basename), "basename");
-
-    some_basename = PathBuf::from(r"/./test/basename");
-    assert_eq!(get_basename(&some_basename), "basename");
-
-    // a filename containing spaces
-    some_basename = PathBuf::from(r"/./test/base name/");
-    assert_eq!(get_basename(&some_basename), "base name");
-}
-
-#[test]
-#[ignore]
-fn check_get_basename_windows() {
-    // a file on Windows
-    let mut some_basename = PathBuf::from(r"C:\windows\system32.dll");
-    // Does not work on Linux (and maybe not on Windows either; returns "C:\\windows\\system32.dll")
-    assert_eq!(get_basename(&some_basename), "system32.dll");
 }
