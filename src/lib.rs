208db9ae8e0ba5ade31d935a77b52dfb477ca2b6
andy5995 <andy400-dev@yahoo.com>
2020-07-29 23:22:23 -0500

    use WASTE from config file; add '-c, --custom-config-file' option
    (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 pub mod libgen;
 
+pub mod waste {
+    pub struct WasteFolderProperties {
+        pub parent: String,
+        pub info: String,
+        pub file: String,
+        pub is_removable: bool,
+    }
+
+    impl WasteFolderProperties {
+        pub fn new() -> Self {
+            Self {
+                parent: String::new(),
+                info: String::new(),
+                file: String::new(),
+                is_removable: false,
+            }
+        }
+    }
+}
+
 pub mod configster {
 
     use std::fs::File;
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ struct Opt {
     #[structopt(short = "V", long = "version")]
     version: bool,
 
+    /// Specify path/filename of alternate configuration file
+    #[structopt(short = "c", long = "config")]
+    custom_config_file: Option<String>,
+
     /// Files to process
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
@@ -55,37 +59,57 @@ fn main() -> Result<(), io::Error> {
         println!();
     }
 
-    let config_vec = oxi_rmw::configster::parse_file("./config_test.conf", ',');
+    let config_file: String;
+    if opt.custom_config_file.is_none() {
+        config_file = "./config_test.conf".to_string();
+    } else {
+        config_file = opt.custom_config_file.unwrap();
+    }
+
+    let config_vec = oxi_rmw::configster::parse_file(&config_file, ',');
     if config_vec.is_err() {
         return io::Result::Err(config_vec.unwrap_err());
     }
 
+    let mut waste_list = Vec::new();
+
     for i in &config_vec.unwrap() {
-        println!("Option:'{}' | value '{}'", i.option, i.value.primary);
+        if i.option == "WASTE" {
+            let mut waste_properties = oxi_rmw::waste::WasteFolderProperties::new();
+            waste_properties.parent = i.value.primary.replace("$HOME", &homedir);
 
-        for j in &i.value.attributes {
-            println!("attr:'{}`", j);
+            waste_properties.info = format!("{}{}", waste_properties.parent, "/info");
+            println!("Using {}", &waste_properties.info);
+            if Path::new(&waste_properties.info).exists() == false {
+                println!("Creating {}", &waste_properties.info);
+                let r = fs::create_dir_all(&waste_properties.info);
+                if r.is_err() {
+                    return io::Result::Err(r.unwrap_err());
+                }
+            }
+
+            waste_properties.file = format!("{}{}", waste_properties.parent, "/files");
+            println!("Using {}", &waste_properties.file);
+            if Path::new(&waste_properties.file).exists() == false {
+                println!("Creating {}", &waste_properties.file);
+                let r = fs::create_dir_all(&waste_properties.file);
+                if r.is_err() {
+                    return io::Result::Err(r.unwrap_err());
+                }
+            }
+
+            if i.value.attributes[0] == "removable".to_string() {
+                waste_properties.is_removable = true;
+            }
+
+            waste_list.push(waste_properties);
         }
-        println!();
-    }
-
-    let waste_info = format!("{}/{}", homedir, ".oxi-rmw-Trash-test/info");
-    println!("Using {}", &waste_info);
-    if Path::new(&waste_info).exists() == false {
-        println!("Creating {}", &waste_info);
-        fs::create_dir_all(&waste_info).expect("Could not create directory");
-    }
-
-    let waste_files = format!("{}/{}", homedir, ".oxi-rmw-Trash-test/files");
-    println!("Using {}", &waste_files);
-    if Path::new(&waste_files).exists() == false {
-        println!("Creating {}", &waste_files);
-        fs::create_dir_all(&waste_files).expect("Could not create directory");
     }
 
     let date_now = Local::now();
     let deletion_date = date_now.format("%Y-%m-%dT%H:%M:%S").to_string();
 
+    let waste = &waste_list[0];
     // The format of the trashinfo file corresponds to that of the FreeDesktop.org
     // Trash specification<https://specifications.freedesktop.org/trash-spec/trashspec-latest.html>.
     for file in &opt.files {
@@ -94,11 +118,12 @@ fn main() -> Result<(), io::Error> {
 
         // Will need more error-checking to prevent overwriting existing destination files.
         // As in the C version of rmw, some type of time/date string is appended in that case.
-        let destination = format!("{}/{}", &waste_files, basename);
+        let destination = format!("{}/{}", &waste.file, basename);
+        println!("'{}' -> '{}'", file.display(), destination);
         rename(file, &destination).expect("Error renaming file");
 
         let contents = trashinfo::create_contents(&file_absolute, &deletion_date);
-        trashinfo::create(&basename, &waste_info, contents).expect("Error writing trashinfo file");
+        trashinfo::create(&basename, &waste.info, contents).expect("Error writing trashinfo file");
     }
     Ok(())
 }
