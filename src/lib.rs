d3944073b47d37ebba2a1a80e82e73bcf2e1387d
andy5995 <andy400-dev@yahoo.com>
2020-08-01 23:24:01 -0500

    break config mod into functions

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,45 @@ pub mod config {
     use std::os::unix::fs::MetadataExt;
     use std::path::Path;
 
+    fn get_filename(opt_cfg: Option<String>) -> String {
+        if opt_cfg.is_none() {
+            return "./config_test.conf".to_string();
+        }
+        opt_cfg.unwrap()
+    }
+
+    fn get_dev_num(wp: &String) -> io::Result<u64> {
+        // Device num not used yet. Used to determine what file system a file is on,
+        // and therefore which waste folder it can be rmw'ed to. (rmw doesn't move or
+        // copy files to different file systems. (Apparently not available on Windows:
+        // https://doc.rust-lang.org/std/os/windows/fs/trait.MetadataExt.html)
+        let meta = fs::metadata(wp)?;
+        Ok(meta.dev())
+    }
+
+    fn assign_properties(
+        i: &configster::OptionProperties,
+        homedir: &String,
+    ) -> io::Result<waste::WasteFolderProperties> {
+        let mut waste_properties = waste::WasteFolderProperties::new();
+        waste_properties.parent = i.value.primary.replace("$HOME", &homedir);
+
+        waste_properties.info = format!("{}{}", waste_properties.parent, "/info");
+        println!("Using {}", &waste_properties.info);
+        if Path::new(&waste_properties.info).exists() == false {
+            println!("Creating {}", &waste_properties.info);
+            fs::create_dir_all(&waste_properties.info)?;
+        }
+
+        waste_properties.file = format!("{}{}", waste_properties.parent, "/files");
+        println!("Using {}", &waste_properties.file);
+        if Path::new(&waste_properties.file).exists() == false {
+            println!("Creating {}", &waste_properties.file);
+            fs::create_dir_all(&waste_properties.file)?;
+        }
+        Ok(waste_properties)
+    }
+
     pub fn parse(
         opt_cfg: Option<String>,
         homedir: String,
@@ -37,12 +76,7 @@ pub mod config {
         Vec<waste::WasteFolderProperties>,
         Vec<configster::OptionProperties>,
     )> {
-        let config_file: String;
-        if opt_cfg.is_none() {
-            config_file = "./config_test.conf".to_string();
-        } else {
-            config_file = opt_cfg.unwrap();
-        }
+        let config_file = get_filename(opt_cfg);
 
         let mut waste_list = Vec::new();
 
@@ -50,33 +84,13 @@ pub mod config {
 
         for i in &config_vec {
             if i.option == "WASTE" {
-                let mut waste_properties = waste::WasteFolderProperties::new();
-                waste_properties.parent = i.value.primary.replace("$HOME", &homedir);
-
-                waste_properties.info = format!("{}{}", waste_properties.parent, "/info");
-                println!("Using {}", &waste_properties.info);
-                if Path::new(&waste_properties.info).exists() == false {
-                    println!("Creating {}", &waste_properties.info);
-                    fs::create_dir_all(&waste_properties.info)?;
-                }
-
-                waste_properties.file = format!("{}{}", waste_properties.parent, "/files");
-                println!("Using {}", &waste_properties.file);
-                if Path::new(&waste_properties.file).exists() == false {
-                    println!("Creating {}", &waste_properties.file);
-                    fs::create_dir_all(&waste_properties.file)?;
-                }
+                let mut waste_properties = assign_properties(&i, &homedir)?;
 
                 if i.value.attributes[0] == "removable".to_string() {
                     waste_properties.is_removable = true;
                 }
 
-                // Device id not used yet. Used to determine what file system a file is on,
-                // and therefore which waste folder it can be rmw'ed to. (rmw doesn't move or
-                // copy files to different file systems. (Apparently not available on Windows:
-                // https://doc.rust-lang.org/std/os/windows/fs/trait.MetadataExt.html)
-                let meta = fs::metadata(&waste_properties.parent)?;
-                waste_properties.dev_num = meta.dev();
+                waste_properties.dev_num = get_dev_num(&waste_properties.parent)?;
 
                 waste_list.push(waste_properties);
             }
