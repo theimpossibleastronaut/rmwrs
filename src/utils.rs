f4f055d4fb0d3fccaa65dc854b12f1fe6c0bebd7
Jammyjamjamman <jdsherratt3@gmail.com>
2020-09-29 01:58:29 +0100

    URI % encode: Add unescape_url
    * URI string can now be unescaped.
    * Tests added.
    * Function signatures now require &str instead of &String,
    for better compatibility.

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,24 @@
-pub fn escape_url(to_encode: &String) -> String {
+pub fn escape_url(to_encode: &str) -> String {
     to_encode.chars()
         .map(|c| percent_encode_reserved(c))
         .fold(String::new(), |acc, s| acc + s.as_str())
+}
+
+pub fn unescape_url(to_decode: &str) -> String {
+    let mut chars = to_decode.chars();
+    let mut result = String::new();
+    while let Some(c) = chars.next() {
+        if c == '%' {
+            let mut char_code = String::new();
+            char_code.push(chars.next().expect("char missing in encoding"));
+            char_code.push(chars.next().expect("char missing in encoding"));
+            result.push(u8::from_str_radix(&char_code, 16).expect("error parsing char code to char") as char);
+        }
+        else {
+            result.push(c);
+        }
+    }
+    result
 }
 
 pub fn percent_encode_reserved(to_encode: char) -> String {
--- tests/rmw_utils.rs
+++ tests/rmw_utils.rs
@@ -2,6 +2,12 @@ use rmwrs::utils;
 
 #[test]
 fn test_escape_url() {
-    assert_eq!(utils::escape_url(&"~/foo/bar&/baz boom".to_owned()),"~/foo/bar%26/baz%20boom");
-    assert_eq!(utils::escape_url(&"~/hello&there/🐢👀🍻".to_owned()), "~/hello%26there/🐢👀🍻");
+    assert_eq!(utils::escape_url("~/foo/bar&/baz boom"),"~/foo/bar%26/baz%20boom");
+    assert_eq!(utils::escape_url("~/hello&there/🐢👀🍻"), "~/hello%26there/🐢👀🍻");
+}
+
+#[test]
+fn test_unescape_url() {
+    assert_eq!(utils::unescape_url("~/foo/bar%26/baz%20boom"), "~/foo/bar&/baz boom");
+    assert_eq!(utils::unescape_url("~/hello%26there/🐢👀🍻"),"~/hello&there/🐢👀🍻");
 }
