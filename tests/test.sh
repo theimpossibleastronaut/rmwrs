f35ef870e6c605735932df81c1d99ecc4f25f521
andy5995 <andy400-dev@yahoo.com>
2021-05-10 15:48:40 -0500

    add tests
    test for restoring using relative and absolute path
    
    test for url encoding and decoding
    
    commented test for appended time string when duplicate exists at
    destination

--- tests/test.sh
+++ tests/test.sh
@@ -8,6 +8,12 @@ if test -r ${RMWRS_TEST_HOME}; then
   rm -rf ${RMWRS_TEST_HOME}
 fi
 
+BASENAME_TEST_WASTE_FOLDER=".rmwrs-Trash-test"
+TEST_WASTE_FOLDER="${RMWRS_TEST_HOME}/${BASENAME_TEST_WASTE_FOLDER}"
+
+CFG_FILE="${CARGO_MANIFEST_DIR}/tests/bin_test.conf"
+OLD_PWD="${PWD}"
+
 test_result_want_fail() {
   set +x
   if [ $1 = 0 ]; then
@@ -19,11 +25,16 @@ test_result_want_fail() {
   set -x
 }
 
-TEST_WASTE_FOLDER="${RMWRS_TEST_HOME}/.rmwrs-Trash-test"
+set -x
+
+mkdir -p "${RMWRS_TEST_HOME}" || exit $?
+cd "${RMWRS_TEST_HOME}" || exit $?
+
+TEST_CMD="${CARGO_MANIFEST_DIR}/target/debug/${CARGO_PKG_NAME}"
 
 touch foo bar || exit $?
 
-cargo run -- -c tests/bin_test.conf foo bar || exit $?
+$TEST_CMD  foo bar || exit $?
 
 test -e ${TEST_WASTE_FOLDER}/files/foo || exit $?
 test -e ${TEST_WASTE_FOLDER}/info/foo.trashinfo || exit $?
@@ -39,22 +50,67 @@ test ! -e foo || exit $?
 
 # https://doc.rust-lang.org/cargo/reference/environment-variables.html
 # CARGO_BUILD_TARGET_DIR was an empty variable when I tried this
-# ${CARGO_BUILD_TARGET_DIR}/${CARGO_PKG_NAME} -c tests/bin_test.conf -v -z ${TEST_WASTE_FOLDER}/files/foo || exit $?
+# ${CARGO_BUILD_TARGET_DIR}/${CARGO_PKG_NAME} -c "${CFG_FILE}" -v -z ${TEST_WASTE_FOLDER}/files/foo || exit $?
 #
 # It would be best if we didn't need to use the literal string "/target/debug/"
 # in the command below.
-${CARGO_MANIFEST_DIR}/target/debug/${CARGO_PKG_NAME} -c tests/bin_test.conf -v -z ${TEST_WASTE_FOLDER}/files/foo || exit $?
+$TEST_CMD -c "${CFG_FILE}" -v -z ${TEST_WASTE_FOLDER}/files/foo || exit $?
 test -e foo || exit $?
 test ! -e ${TEST_WASTE_FOLDER}/files/foo || exit $?
 test ! -e ${TEST_WASTE_FOLDER}/info/foo.trashinfo || exit $?
+
+# restore 'bar' for next test
+$TEST_CMD -c "${CFG_FILE}" -v -z ${TEST_WASTE_FOLDER}/files/bar || exit $?
+
+$TEST_CMD -c "${CFG_FILE}" foo bar || exit $?
+set +x
+echo "\n\nTest restore using absolute path"
+set -x
+
+$TEST_CMD -c "${CFG_FILE}" -z "${TEST_WASTE_FOLDER}/files/"* || exit $?
+test -e foo || exit $?
+test -e bar || exit $?
+
+$TEST_CMD -c "${CFG_FILE}" foo bar || exit $?
+
+set +x
+echo "\n\ntest restore using relative path"
+echo "pwd is ${PWD}\n\n"
+set -x
+
+$TEST_CMD -c "${CFG_FILE}" -z "${BASENAME_TEST_WASTE_FOLDER}/files/foo" || exit $?
+test -e "foo" || exit $?
+
+$TEST_CMD -c "${CFG_FILE}" foo || exit $?
+# test restore using wildcard
+
+$TEST_CMD -c "${CFG_FILE}" -z "${BASENAME_TEST_WASTE_FOLDER}/files/"* || exit $?
+test -e "foo" || exit $?
+test -e "bar" || exit $?
+
+# filenames with spaces
+touch "any bar"
+$TEST_CMD -c "${CFG_FILE}" "any bar" || exit $?
+# The space should have been converted to "%20"
+substring="any%20bar"
+output=$(cat "${BASENAME_TEST_WASTE_FOLDER}/info/any bar.trashinfo") || exit $?
+test "${output#*$substring}" != "$output" || exit $?
+$TEST_CMD -c "${CFG_FILE}" -z "${BASENAME_TEST_WASTE_FOLDER}/files/any bar" || exit $?
+test -e "any bar" || exit $?
+
+# When restoring, file should have time/date string (formatted as
+# "_%H%M%S-%y%m%d") appended to it (e.g. 'foo_164353-210508').
+#$TEST_CMD -c "${CFG_FILE}" foo || exit $?
+#touch foo
+#$TEST_CMD -c "${CFG_FILE}" -z "${BASENAME_TEST_WASTE_FOLDER}/files/foo" || exit $?
+#ls foo_* || exit $?
 
 if test -r ${RMWRS_TEST_HOME}; then
   rm -rf ${RMWRS_TEST_HOME}
 fi
 
-
 # Check for invalid attribute in the Waste file (want fail)
-cargo run -- -c tests/bin_test_invalid.conf
+$TEST_CMD -c tests/bin_test_invalid.conf
 test_result_want_fail $?
 
 if test -r ${RMWRS_TEST_HOME}; then
